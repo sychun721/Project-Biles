91ef8113625ef47ed87287d89ba6cf767b54c22a
sychun721 <gloriachun@college.harvard.edu>
2024-11-29 15:44:40 -0500

    final output

--- Regression.R
+++ Regression.R
@@ -1,1 +1,13 @@
 
+include <ft2build.h>
+library(tidyverse)
+library(readr)
+library(readxl)
+library(ggplot2)
+data <- read_excel("Gross New MRR.xlsx")
+
+# View the first few rows of the dataset
+head(data)
+
+# Check the structure of the data
+str(data)
